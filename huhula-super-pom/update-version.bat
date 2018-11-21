135a2c4464c87f4108382ab15f54a5514dd08198
João Carlos Brasileiro <j.brasileiro@sap.com>
2018-11-21 18:41:58 -0200

    update script.

--- huhula-super-pom/update-version.bat
+++ huhula-super-pom/update-version.bat
@@ -1,1 +1,21 @@
-mvn versions:set -DnewVersion=J7.1.1.3
+@ECHO OFF
+SETLOCAL
+::START
+
+::SET VARIABLE
+SET VERSION_RELEASE=J7.1.2.2
+
+::STARTING
+echo executing in current dir "%~dp0"
+CD "%~dp0"
+
+::update version fro all current related project
+echo updating version in all modules to %VERSION_RELEASE%
+cmd /C mvn versions:set -DnewVersion=%VERSION_RELEASE% -DprocessAllModules
+::PAUSE
+
+::deploy and release
+mvn clean deploy -P release
+
+::END
+ENDLOCAL
