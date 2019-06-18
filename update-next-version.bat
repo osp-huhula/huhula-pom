5f0df539f48478bd0ee9b3954802baff6560a869
João Carlos Brasileiro <jcbrasileiro@hotmail.com>
2019-06-17 21:53:30 -0300

    update pom.xml

--- update-next-version.bat
+++ update-next-version.bat
@@ -6,12 +6,19 @@ SET VERSION=J7.1.2.3
 SET NEXT_VERSION=J7.1.2.4-SNAPSHOT
 
 
-SET CMVN=cmd /C mvn -q
+SET CMVN=cmd /C mvn
 SET CMVN_SUPER_POM=%CMVN% -f huhula-super-pom\pom.xml
 
 ::STARTING
 echo executing in current dir "%~dp0"
 CD "%~dp0"
+
+%CMVN_SUPER_POM% release:clean --batch-mode release:prepare -Dresume=false -Dtag=%VERSION% -DreleaseVersion=%VERSION% -DdevelopmentVersion=%NEXT_VERSION%
+
+IF NOT %ERRORLEVEL% EQU 0 (
+   echo Failure Reason Given is %errorlevel%
+   GOTO:ROLLBACK
+)
 
 %CMVN_SUPER_POM% versions:set -DnewVersion=%VERSION%
 
@@ -20,45 +27,39 @@ IF NOT %ERRORLEVEL% EQU 0 (
    GOTO:ROLLBACK
 )
 
-%CMVN_SUPER_POM% -N versions:update-child-modules
+IF NOT %ERRORLEVEL% EQU 0 (
+   echo Failure Reason Given is %errorlevel%
+   GOTO:ROLLBACK
+)
+
+%CMVN_SUPER_POM% clean install deploy site
 
 IF NOT %ERRORLEVEL% EQU 0 (
    echo Failure Reason Given is %errorlevel%
    GOTO:ROLLBACK
 )
 
-%CMVN_SUPER_POM% clean install
+%CMVN% clean install deploy site
 
 IF NOT %ERRORLEVEL% EQU 0 (
    echo Failure Reason Given is %errorlevel%
    GOTO:ROLLBACK
 )
 
-%CMVN% clean install
+
+%CMVN% versions:revert
 
 IF NOT %ERRORLEVEL% EQU 0 (
    echo Failure Reason Given is %errorlevel%
    GOTO:ROLLBACK
 )
 
-%CMVN_SUPER_POM% release:clean -B --batch-mode release:prepare -Dresume=false -Dtag=%VERSION% -DreleaseVersion=%VERSION% -DdevelopmentVersion=%NEXT_VERSION%
-
-IF NOT %ERRORLEVEL% EQU 0 (
-   echo Failure Reason Given is %errorlevel%
-   GOTO:ROLLBACK
-)
-
-%CMVN_SUPER_POM% -N versions:update-child-modules
-
-IF NOT %ERRORLEVEL% EQU 0 (
-   echo Failure Reason Given is %errorlevel%
-   GOTO:ROLLBACK
-)
-
+@REM %CMVN% release:perform
+GOTO:ROLLBACK
 GOTO:EOF
 
 :ROLLBACK
-%CMVN% release:rollback
+%CMVN% release:rollback 
 %CMVN% versions:revert
 exit /b %errorlevel%
 GOTO:EOF
