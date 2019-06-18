94a8ac6d038e23716f209f136b9eda51b1885718
João Carlos Brasileiro <jcbrasileiro@hotmail.com>
2019-06-18 09:42:01 -0300

    update release process (bat)

--- update-next-version.bat
+++ update-next-version.bat
@@ -6,14 +6,13 @@ SET VERSION=J7.1.2.3
 SET NEXT_VERSION=J7.1.2.4-SNAPSHOT
 
 
-SET CMVN=cmd /C mvn -Dmvn.antrun.config.echoproperties=true
+SET CMVN=cmd /C mvnw -s C:\.rep\git\P\software-architecture\config\maven\settings.xml -Dmvn.antrun.config.echoproperties=true
 SET CMVN_SUPER_POM=%CMVN% -f huhula-super-pom\pom.xml
 
 ::STARTING
 echo executing in current dir "%~dp0"
 CD "%~dp0"
 
-%CMVN_SUPER_POM% release:clean --batch-mode release:prepare -Dresume=false -Dtag=%VERSION% -DreleaseVersion=%VERSION% -DdevelopmentVersion=%NEXT_VERSION%
 
 IF NOT %ERRORLEVEL% EQU 0 (
    echo Failure Reason Given is %errorlevel%
@@ -32,21 +31,22 @@ IF NOT %ERRORLEVEL% EQU 0 (
    GOTO:ROLLBACK
 )
 
-%CMVN_SUPER_POM% clean install deploy site
+%CMVN_SUPER_POM% clean deploy site
 
 IF NOT %ERRORLEVEL% EQU 0 (
    echo Failure Reason Given is %errorlevel%
    GOTO:ROLLBACK
 )
 
-%CMVN% clean install deploy site
+%CMVN% clean deploy site
 
 IF NOT %ERRORLEVEL% EQU 0 (
    echo Failure Reason Given is %errorlevel%
    GOTO:ROLLBACK
 )
 
-
+@REM
+GOTO:ROLLBACK
 %CMVN_SUPER_POM% versions:revert
 
 IF NOT %ERRORLEVEL% EQU 0 (
@@ -61,6 +61,8 @@ GOTO:EOF
 :ROLLBACK
 %CMVN_SUPER_POM% -q release:rollback 
 %CMVN_SUPER_POM% -q versions:revert
+%CMVN% -q release:rollback 
+%CMVN% -q versions:revert
 exit /b %errorlevel%
 GOTO:EOF
 
