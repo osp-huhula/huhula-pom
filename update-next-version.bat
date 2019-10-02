9363dd6fa0548570fc4dc7a0e38dfeb99c960c28
João Carlos Brasileiro <jcbrasileiro@hotmail.com>
2019-10-01 23:08:03 -0300

    add enforce for release plugging (Profile RELEASE) update BAT file

--- update-next-version.bat
+++ update-next-version.bat
@@ -31,6 +31,26 @@ IF NOT %ERRORLEVEL% EQU 0 (
 )
 
 echo ##########################################
+echo executing update version super-POM (versions:set)
+echo ########################################## 
+%CMVN_SUPER_POM% versions:set -DnewVersion=%VERSION%
+
+IF NOT %ERRORLEVEL% EQU 0 (
+   echo Failure Reason Given is %errorlevel%
+   GOTO:ROLLBACK
+)
+
+echo ##########################################
+echo executing update version (versions:set)
+echo ########################################## 
+%CMVN_SUPER_POM% versions:revert -DnewVersion=%VERSION%
+
+IF NOT %ERRORLEVEL% EQU 0 (
+   echo Failure Reason Given is %errorlevel%
+   GOTO:ROLLBACK
+)
+
+echo ##########################################
 echo executing mvn process in super-pom
 echo ##########################################
 %CMVN_SUPER_POM% clean deploy site
@@ -64,6 +84,10 @@ echo ##########################################
 echo executing simulation release 
 echo ##########################################
 %CMVN% release:clean release:prepare-with-pom -Dresume=false -DdryRun=true --batch-mode -DautoVersionSubmodules=true -Dtag=%VERSION% -DreleaseVersion=%VERSION% -DdevelopmentVersion=%NEXT_VERSION%
+
+::%CMVN_SUPER_POM% versions:set -DnewVersion=%VERSION%
+::%CMVN% release:clean release:prepare-with-pom -Dresume=false -DdryRun=false --batch-mode -DautoVersionSubmodules=true -Dtag=%VERSION% -DreleaseVersion=%VERSION% -DdevelopmentVersion=%NEXT_VERSION%
+%CMVN% mvn clean deploy site -PRELEASE -DreleaseVersion=%VERSION% -DdevelopmentVersion=%NEXT_VERSION%
 
 IF NOT %ERRORLEVEL% EQU 0 (
    echo Failure Reason Given is %errorlevel%
