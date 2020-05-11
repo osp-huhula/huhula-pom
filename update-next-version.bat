d2c796c3f3ee684868980a6eee3f6cd731b44c8e
João Brasileiro <jcbrasileiro@hotmail.com>
2020-05-11 00:14:29 -0300

    Update POM

--- update-next-version.bat
+++ update-next-version.bat
@@ -2,59 +2,58 @@
 SETLOCAL
 
 ::SET VARIABLE
-SET VERSION=J7.1.3.1
-SET NEXT_VERSION=J7.1.3.2-SNAPSHOT
+::SET VERSION=J7.1.3.1
+::SET NEXT_VERSION=J7.1.3.2-SNAPSHOT
 
 
-SET CMVN=cmd /C mvnw -s %MVN_HOME%\conf\settings.xml -Dmvn.antrun.config.echoproperties=true
+SET CMVN=cmd /C mvnw -s %MVN_HOME%\conf\settings.xml
 SET CMVN_SUPER_POM=%CMVN% -f huhula-super-pom\pom.xml
 
 ::STARTING
 echo executing in current dir "%~dp0"
 CD "%~dp0"
-
-
 IF NOT %ERRORLEVEL% EQU 0 (
    echo Failure Reason Given is %errorlevel%
    GOTO:ROLLBACK
 )
 
-%CMVN_SUPER_POM% -q versions:set -DnewVersion=%VERSION% -DprocessAllModules
-
+%CMVN_SUPER_POM% versions:set -DnewVersion=0.0.0 -DprocessAllModules
 IF NOT %ERRORLEVEL% EQU 0 (
    echo Failure Reason Given is %errorlevel%
    GOTO:ROLLBACK
 )
 
-%CMVN_SUPER_POM% -q clean deploy site
-
+%CMVN_SUPER_POM% clean deploy site -PSKIP-ASC
 IF NOT %ERRORLEVEL% EQU 0 (
    echo Failure Reason Given is %errorlevel%
    GOTO:ROLLBACK
 )
 
-%CMVN% -q clean deploy site
-
+%CMVN% clean deploy site -PSKIP-ASC
 IF NOT %ERRORLEVEL% EQU 0 (
    echo Failure Reason Given is %errorlevel%
    GOTO:ROLLBACK
 )
 
 %CMVN% versions:revert
-
 IF NOT %ERRORLEVEL% EQU 0 (
    echo Failure Reason Given is %errorlevel%
    GOTO:ROLLBACK
 )
 
-%CMVN% -q release:clean release:prepare release:perform --batch-mode -Dresume=false -DdryRun=true -Dtag=%VERSION% -DreleaseVersion=%VERSION% -DdevelopmentVersion=%NEXT_VERSION%
-%CMVN% -q release:clean
-%CMVN% -q release:rollback 
-%CMVN% -q release:clean release:prepare release:perform --batch-mode -Dresume=false -DdryRun=false -Dtag=%VERSION% -DreleaseVersion=%VERSION% -DdevelopmentVersion=%NEXT_VERSION%
-
-%CMVN% clean deploy -PRELEASE
+::%CMVN% -q release:clean release:prepare release:perform --batch-mode -Dresume=false -DdryRun=true -Dtag=%VERSION% -DreleaseVersion=%VERSION% -DdevelopmentVersion=%NEXT_VERSION%
+::%CMVN% -q release:clean
+::%CMVN% -q release:rollback 
+::%CMVN% -q release:clean release:prepare release:perform --batch-mode -Dresume=false -DdryRun=false -Dtag=%VERSION% -DreleaseVersion=%VERSION% -DdevelopmentVersion=%NEXT_VERSION%
+::%CMVN% clean deploy -PRELEASE
 
 GOTO:EOF
+
+:VERIFY_ERROR
+IF NOT %ERRORLEVEL% EQU 0 (
+   echo Failure Reason Given is %errorlevel%
+   GOTO:ROLLBACK
+)
 
 :ROLLBACK
 %CMVN_SUPER_POM% -q release:rollback 
