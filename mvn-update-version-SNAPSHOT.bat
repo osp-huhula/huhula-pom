b54c26309cc5c16ccec4157fad19eff46d4997ea
João Brasileiro <jcbrasileiro@hotmail.com>
2020-05-11 20:44:21 -0300

    update to next timestamp SNAPSHOT version.

--- mvn-update-version-SNAPSHOT.bat
+++ mvn-update-version-SNAPSHOT.bat
@@ -1,6 +1,10 @@
 @ECHO OFF
 SETLOCAL
 
-mvn validate -PVERSION_TIMESTAMP_SNAPSHOT,UPDATE_RELEASE_RELEASE
-
+cmd /C mvn validate -PVERSION_TIMESTAMP_SNAPSHOT,UPDATE_RELEASE_RELEASE
+git status
+git add */pom.xml
+git status
+git commit -m "update to next timestamp SNAPSHOT version."
+git push
 ENDLOCAL
