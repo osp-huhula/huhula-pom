f2f16aee0bba18646bb59161f99c70cdf18ef375
João Brasileiro <jcbrasileiro@hotmail.com>
2020-05-11 02:22:40 -0300

    update POM update BAT

--- update-next-version.bat
+++ update-next-version.bat
@@ -17,7 +17,7 @@ IF NOT %ERRORLEVEL% EQU 0 (
    GOTO:ROLLBACK
 )
 
-%CMVN_SUPER_POM% -q versions:set -PTIMESTAMP -DprocessAllModules
+%CMVN_SUPER_POM% -q versions:set -DnewVersion=0.0.0-SNAPSHOT -DprocessAllModules
 IF NOT %ERRORLEVEL% EQU 0 (
    echo Failure Reason Given is %errorlevel%
    GOTO:ROLLBACK
