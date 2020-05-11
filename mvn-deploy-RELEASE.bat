829d840871d3ae47336d6a006ab7e2379d63bbc1
João Brasileiro <jcbrasileiro@hotmail.com>
2020-05-11 20:08:18 -0300

    update POM

--- mvn-deploy-RELEASE.bat
+++ mvn-deploy-RELEASE.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 SETLOCAL
 
-mvn clean deploy site -B -PSKIP-ASC -DPROFILE_RELEASE=RELEASE -Dmvn-base-version-release=J7.1.4.0 > c:\.tmp\log\mvn-deploy-RELEASE.log
+mvn clean deploy site -B -PSKIP-ASC -DPROFILE_RELEASE=RELEASE > c:\.tmp\log\mvn-deploy-RELEASE.log
 
 ENDLOCAL
