baa685a39c677d043fe7736208c8b430b8f93949
João Carlos Brasileiro <jcbrasileiro@hotmail.com>
2026-03-22 13:23:12 -0300

    update to next timestamp SNAPSHOT version.

--- mvn-deploy-SNAPSHOT.bat
+++ mvn-deploy-SNAPSHOT.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
 SETLOCAL
+REM Requires Central Publisher Portal user token in ~/.m2/settings.xml for server id "sonatype-snapshot"
+REM (https://central.sonatype.org/publish/generate-portal-token/) and SNAPSHOTs enabled for the namespace.
 
 mvn clean deploy site -B -PSKIP-ASC -DPROFILE_RELEASE=SNAPSHOT  > c:\.tmp\log\mvn-deploy-SNAPSHOT.log
 
