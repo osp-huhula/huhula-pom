de987932c1a53f8df001bc70b3f944089572d061
João Carlos Brasileiro <jcbrasileiro@hotmail.com>
2026-03-22 20:59:12 -0300

    update version

--- update-next-version.bat
+++ update-next-version.bat
@@ -57,10 +57,10 @@ IF NOT %ERRORLEVEL% EQU 0 (
 GOTO:EOF
 
 :ROLLBACK
-%CMVN_SUPER_POM% -q release:rollback 
+::%CMVN_SUPER_POM% -q release:rollback 
 %CMVN_SUPER_POM% -q versions:revert
-%CMVN% -q release:rollback 
-::%CMVN% -q versions:revert
+::%CMVN% -q release:rollback 
+%CMVN% -q versions:revert
 exit /b %errorlevel%
 GOTO:EOF
 
